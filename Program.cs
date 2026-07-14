067b29f999499f790501aadc21fa070f6c6441f5
Eshraq <eshraq416@gmail.com>
2026-07-14 16:50:14 +0400

    Case 2 - Update Student Address

--- Program.cs
+++ Program.cs
@@ -10,3 +10,19 @@ public static class Case01
         chosen.CheckBalance();
     }
 }
+
+
+// Case 2 - Update Student Address
+public static class Case02
+{
+    public static void Run(Student s1, Student s2)
+    {
+        Console.WriteLine("--- Case 2: Update Student Address ---");
+        Student chosen = InputHelper.ChooseStudent(s1, s2);
+        Console.Write("Enter new address: ");
+        string newAddress = Console.ReadLine()!;
+
+        chosen.Address = newAddress;
+        Console.WriteLine($"Address updated. {chosen.Name}'s new address is: {chosen.Address}");
+    }
+}
